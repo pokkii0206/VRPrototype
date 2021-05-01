ecbcac47e492868686ffb617df8e2f509f272fed
takaaki yamana <pokky0206@gmail.com>
2021-05-01 21:50:33 +0900

    test commit

--- Assets/AdProject/Scripts/BoxInteract.cs
+++ Assets/AdProject/Scripts/BoxInteract.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+///aaa
 public class BoxInteract : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
 {
 
